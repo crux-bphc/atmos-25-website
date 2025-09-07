eea15c1341b7ace38ae0054c908069bee8146a6f
aten2005 <gogo.june2005@gmail.com>
2025-09-07 17:21:12 +0530

    feat: implemented tape component

---
+++ src/components/Tape.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const words = [
+  '7 / 11 FRI - 9 / 11 SUN, HYD',
+  '7 / 11 FRI - 9 / 11 SUN, HYD',
+  '7 / 11 FRI - 9 / 11 SUN, HYD',
+  '7 / 11 FRI - 9 / 11 SUN, HYD',
+  '7 / 11 FRI - 9 / 11 SUN, HYD',
+  '7 / 11 FRI - 9 / 11 SUN, HYD',
+  '7 / 11 FRI - 9 / 11 SUN, HYD',
+  '7 / 11 FRI - 9 / 11 SUN, HYD',
+  '7 / 11 FRI - 9 / 11 SUN, HYD',
+];
+
+const Tape = () => {
+  return (
+    <div className="overflow-x-clip overflow-y-hidden pb-32 pt-16 lg:py-24">
+      <div className="-mx-1 -rotate-2 bg-black">
+        <div className="flex">
+          <div className="flex flex-none animate-move-left gap-4 py-3 pr-4">
+            {[...new Array(2)].fill(0).map((_, index) => (
+              <React.Fragment key={index}>
+                {words.map((word, wordIndex) => (
+                  <div key={wordIndex} className="inline-flex items-center gap-4">
+                    <span className="text-4xl font-[Helvetica] font-bold uppercase text-white">
+                      {word}
+                    </span>
+										<span className="text-4xl font-[Helvetica] font-bold uppercase text-white">
+											•
+										</span>
+                  </div>
+                ))}
+              </React.Fragment>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Tape;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import logo from "@/assets/logo.png";
 import bg_image from "@/assets/home_bg.png"
+import Tape from "@/components/Tape";
 
 export const Route = createFileRoute("/")({
 	component: RouteComponent,
@@ -8,9 +9,21 @@ export const Route = createFileRoute("/")({
 
 function RouteComponent() {
 	return (
-		<div className="flex flex-col w-screen h-screen justify-start items-center overflow-x-hidden">
-			<img src={bg_image} className="w-screen h-screen p-0 m-0 absolute left-0 top-0 -z-1 object-cover object-top" />
-			<img className="w-[30rem] mt-48" src={logo} alt="atmos solaris surge" />
-		</div>
+		<>
+			<div className="flex flex-col w-screen h-screen justify-start items-center overflow-x-hidden">
+				<img src={bg_image} className="w-screen h-screen p-0 m-0 absolute left-0 top-0 -z-1 object-cover object-top" />
+				<img className="w-[30rem] mt-48" src={logo} alt="atmos solaris surge" />
+				<Tape/>
+			</div>
+			<div className="flex flex-col w-screen h-screen justify-start items-center overflow-x-hidden">
+				Slide2
+			</div>
+			<div className="flex flex-col w-screen h-screen justify-start items-center overflow-x-hidden">
+				Slide3
+			</div>
+			<div className="flex flex-col w-screen h-screen justify-start items-center overflow-x-hidden">
+				Slide4
+			</div>
+		</>
 	);
 }
