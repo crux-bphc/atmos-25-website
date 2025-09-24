03a714e6e070d1ae9925227260d077d11091dbfe
aten2005 <gogo.june2005@gmail.com>
2025-09-24 23:20:27 +0530

    fix: fixed typescript error

--- src/components/ProshowsSection.tsx
+++ src/components/ProshowsSection.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useRef } from "react"
 import {gsap} from "gsap"
 import {ScrollTrigger} from "gsap/ScrollTrigger"
-import EventLink from "./EventLink";
 
 gsap.registerPlugin(ScrollTrigger);
 
