64793352c098851efaa13d5ac5b62a971ebcbc53
unknown <anirudh.mss@gmail.com>
2025-09-29 16:17:13 +0530

    fix: remove proshow component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -23,12 +23,7 @@ function RouteComponent() {
         <div id="headliners" className="snap-center flex flex-col w-screen justify-start items-center overflow-x-hidden bg-[#F5FF46]">
           <Headliners />
         </div>
-        <div id="events" className="snap-start h-screen flex flex-col w-screen justify-start overflow-x-hidden p-0 m-0">
-          <EventsSection/>
-        </div>
-        <div className="snap-start h-screen flex flex-col w-screen justify-start overflow-x-hidden p-0 m-0">
-          <ProshowsSection/>
-        </div>
+        <EventsSection/>
         <Gallery/>
         <Footer/>
       </div>
