fe80271e2620ec051b866e55ea9e222e2b6d8293
aten2005 <gogo.june2005@gmail.com>
2025-09-29 15:46:26 +0530

    feat: implemented framer animations

--- src/components/Glass.tsx
+++ src/components/Glass.tsx
@@ -1,6 +1,6 @@
 const Glass = ({className = "", style={}}) => {
 	return (
-		<div style={style} className={`bg-[rgba(255,255,255,0.05)] backdrop-blur-[4.4px] [box-shadow:0_4px_30px_rgba(0,0,0,0.03)] -z-2 position absolute ${className}`}/>
+		<div style={style} className={`bg-[rgba(255,255,255,0.05)] backdrop-blur-[4.4px] [box-shadow:0_4px_30px_rgba(0,0,0,0.03)] z-0 absolute ${className}`}/>
 	)
 }
 
