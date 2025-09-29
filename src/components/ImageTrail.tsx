965e66c9cc120b1ad30d076dbf042b89df0dadbe
Swastik Goswami <cwswastik2005@gmail.com>
2025-09-29 12:35:51 +0530

    chore: add more images

--- src/components/ImageTrail.tsx
+++ src/components/ImageTrail.tsx
@@ -1202,7 +1202,7 @@ export default function ImageTrail({ items = [], variant = 1 }: ImageTrailProps)
     <div className="w-full h-full relative z-[100] rounded-lg bg-transparent overflow-visible" ref={containerRef}>
       {items.map((url, i) => (
         <div
-          className="content__img w-[300px] aspect-[1.5] rounded-[2px] absolute top-0 left-0 opacity-0 overflow-hidden [will-change:transform,filter]"
+          className="content__img w-[300px] aspect-[1.1] rounded-[2px] absolute top-0 left-0 opacity-0 overflow-hidden [will-change:transform,filter]"
           key={i}
         >
           <div
