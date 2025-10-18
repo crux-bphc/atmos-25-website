94574c914d0d0b163d96109d10e605f8ca4afcda
aten2005 <gogo.june2005@gmail.com>
2025-10-19 00:08:38 +0530

    feat: added movement to tiles and changed EVENTS colour

--- src/components/Glass.tsx
+++ src/components/Glass.tsx
@@ -1,7 +1,15 @@
-const Glass = ({className = "", style={}}) => {
+import { motion } from "motion/react";
+
+interface GlassProps {
+	speed: number;
+	className?: string;
+	style: Record<string, string>;
+}
+
+const Glass = ({speed, className = "", style={}} : GlassProps) => {
 	return (
-		<div style={style} className={`bg-[rgba(255,255,255,0.05)] backdrop-blur-[4.4px] [box-shadow:0_4px_30px_rgba(0,0,0,0.03)] z-0 absolute ${className}`}/>
+		<motion.div style={style} initial={{y:-500}} animate={{y:"100vh"}} transition={{duration: 50/speed, repeat: Infinity}} className={`bg-[rgba(255,255,255,0.05)] backdrop-blur-[4.4px] [box-shadow:0_4px_30px_rgba(0,0,0,0.03)] z-0 absolute ${className}`}/>
 	)
 }
 
-export default Glass;
+export default Glass
