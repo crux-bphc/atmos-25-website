d5d863bab03a586db5961680b7d97c499530d85b
aten2005 <gogo.june2005@gmail.com>
2025-10-21 21:07:51 +0530

    chore: added placeholder sponsors

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -10,7 +10,10 @@ export default function Sponsors() {
       <h2 className="text-5xl text-center text-white mb-12">
         Our Sponsors
       </h2>
-
+      <h2 className="text-3xl text-center text-white opacity-80 mb-12">
+        Coming soon...
+      </h2>
+      {/* 
       <div className="max-w-6xl mx-auto grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-8 place-items-center">
         {images.map((src, i) => (
           <img
@@ -20,8 +23,8 @@ export default function Sponsors() {
             className="w-40 h-24 object-contain transition duration-300"
             loading="lazy"
           />
-        ))}
-      </div>
+        ))} 
+      </div> */}
     </section>
   );
 }
