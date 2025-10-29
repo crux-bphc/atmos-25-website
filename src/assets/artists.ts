2e964e8e7ef7a425038da0e44565ffac6d7adc58
Jatin Reddy <jatinreddy@Jatins-Laptop.local>
2025-10-30 04:13:54 +0530

    feat: Proshows Section

---
+++ src/assets/artists.ts
@@ -0,0 +1,34 @@
+// src/artists.ts
+import Flame1 from "../assets/proshows/Proshows_01_export.png";
+import Flame2 from "../assets/proshows/Proshows_02_export.png";
+import Mithoon from "../assets/proshows/mithoon.png";
+import Carnivore from "../assets/proshows/Carnivore.png"; 
+
+
+export const artists = [
+  {
+    id: 1,
+    name: "Mithoon",
+    title: "Bollywood Night",
+    date: "8.11.Sat", 
+    time: "8:30pm",
+    location: "BITS Hyd",
+    venue: "Library Lawns",
+    image: Mithoon,
+    flameA: Flame1,
+    flameB: Flame2,
+  },
+  {
+    id: 2,
+    name: "DJ Carnivore",
+    title: "EDM Night",
+    date: "8.11.Sat",
+    time: "9:30pm",
+    location: "BITS Hyd",
+    venue: "Library Lawns",
+    image: Carnivore,
+    flameA: Flame1,
+    flameB: Flame2,
+  },
+  
+];
